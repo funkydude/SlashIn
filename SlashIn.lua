adfe8b806eca3d956e68682ce9b343ed45bea054
Morsk <morsk.warcraft@gmail.com>
2011-05-01 02:57:51 -0400

    cleanup

--- SlashIn.lua
+++ SlashIn.lua
@@ -27,19 +27,10 @@
 local addonName, SlashIn = ...
 SlashIn.ScheduleTimer = LibStub("AceTimer-3.0").ScheduleTimer
 
--- GLOBALS: _G
+local print = print
 local tonumber = tonumber
 local MacroEditBox = MacroEditBox
 local MacroEditBox_OnEvent = MacroEditBox:GetScript("OnEvent")
-
--- Print with colored prefix.
-local print
-do
-	local printPrefix = "|cff33ff99"..addonName.."|r:"
-	function print(...)
-		_G.print(printPrefix, ...)
-	end
-end
 
 -- We execute lines by faking them as EXECUTE_CHAT_LINE events to the MacroEditBox defined in ChatFrame.lua.
 -- The main benefit of this is that MacroEditBox gets special treatment in ChatEdit_OnEscapePressed.
@@ -61,8 +52,9 @@ function SlashCmdList.SLASHIN_IN(msg)
 	local secs, command = msg:match("^([^%s]+)%s+(.*)$")
 	secs = tonumber(secs)
 	if (not secs) or (#command == 0) then
-		print("usage:\n /in <seconds> <command>")
-		print("example:\n /in 1.5 /say hi")
+		local prefix = "|cff33ff99"..addonName.."|r:"
+		print(prefix, "usage:\n /in <seconds> <command>")
+		print(prefix, "example:\n /in 1.5 /say hi")
 	else
 		SlashIn:ScheduleTimer(OnCallback, secs, command)
 	end
